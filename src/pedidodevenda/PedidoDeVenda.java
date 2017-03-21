bcb80a2621717cb728e797c3c5ab174a1b60d084
Natan <natan_ricini@hotmail.com>
2017-03-21 20:12:31 -0300

    Commit inicial

---
+++ src/pedidodevenda/PedidoDeVenda.java
@@ -0,0 +1,17 @@
+
+package pedidodevenda;
+
+/**
+ *
+ * @author Natan
+ */
+public class PedidoDeVenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
